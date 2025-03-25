50c4bd539d88a4807e27769d14652eaa1e25e22d
sangam-cm <sangam@cloudmagician.in>
2025-03-25 19:11:28 +0530

    payninja

--- src/app/member/dashboard/services/payout/page.tsx
+++ src/app/member/dashboard/services/payout/page.tsx
@@ -30,7 +30,7 @@ const page = () => {
 
   const [formData, setFormData] = useState<any>(null);
   const [provider, setProvider] = useState<string | number | boolean>(
-    "cashfree"
+    "payninja"
   );
   const [availableProviders, setAvailableProviders] = useState<any>([]);
 
